bfd50b7b58c1779838c1010e02df94b1300bcec7
subin basnet <Basnetsubeen>
2022-07-14 13:35:52 +1000

    updated lastest code

--- backend/src/config/dbconfig.js
+++ backend/src/config/dbconfig.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 export const dbConnect = () => {
   try {
-    const MONGO_CLIENT = "mongodb://localhost/march_task_list";
-    const conn = mongoose.connect(MONGO_CLIENT);
+    //mongodb+srv://sbaAdmin:<Basnet>@cluster0.83cpm.mongodb.net/march_task_list?retryWrites=true&w=majority
+    // const MONGO_CLIENT = "mongodb://localhost/march_task_list";
+    const conn = mongoose.connect(process.env.MONGO_CLIENT);
     conn && console.log("MongoDB connected");
   } catch (error) {
     console.log(error);
